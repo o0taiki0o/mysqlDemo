26482d405b13ba6bf3c975abef1f2fc08063ba54
Ta Duy Anh <black.cat.3007@gmail.com>
2012-06-11 16:16:47 +0700

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MysqlDemo::Application.config.secret_token = '63461296f83652e0d36d1d92250f3d1e2c6c786ac40ce5b6970b3299c919199cfdbf70cb11b9d17e092af7864648d08aac7b4c5601e1c41fb2d562db0e7c6a06'
